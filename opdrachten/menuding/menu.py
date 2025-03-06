5a0182a7fbe49cdfa0e85fa8df7638198fb3f4b6
SaffyWaffy <124143607+SaffyWaffy@users.noreply.github.com>
2025-03-06 11:28:43 +0100

    pus

--- opdrachten/menuding/menu.py
+++ opdrachten/menuding/menu.py
@@ -7,8 +7,11 @@
 
 def menu():
     print("=================================")
-    print("1: ")
+    print("1: weer dingen ")
+    print("2: random fact")
+    print("3: is de herhaling van deze opdracht dom en zouden we getest moeten worden over python?")
+    print("vul q in om het programa te beindigen")
     print("=================================")
-    print("=================================")
-    print("=================================")
-    print("=================================")
+    keyInput = input()
+
+    
